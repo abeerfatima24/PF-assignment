2f141ba3fb88bd0b46853f8f5e2db9e90601ac8a
abeerfatima24 <abeerkhan2415@gmail.com>
2026-01-11 22:01:30 +0500

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class percentage
+{
+public static void main (String[]args)
+{
+
+    int OM ,MM;
+    double P;
+    Scanner x;
+    x = new Scanner(System.in);
+    System.out.println("Enter Obtained Marks");
+    OM = x.nextInt();
+
+    MM = 1100;
+     P= 1.0 * OM / MM*100;
+
+     System.out.println("percentage: " + P);
+}
+
+    
+}
